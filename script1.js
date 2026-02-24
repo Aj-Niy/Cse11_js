82f5edea0b0b43f26fb45929f7f7ee62cbb9bbdd
Ajit Pandey <ajitpandey4572@gmail.com>
2026-02-24 09:43:14 +0530

    Add initial JavaScript code with variable types, functions, arrays, objects, and symbols

--- script1.js
+++ script1.js
@@ -1,2 +1,51 @@
 console.log("Hello, World!");
 document.write("This is a simple JavaScript file.");
+
+
+let num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="hello bro";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(typeof(num));
+console.log(typeof(num));
+let x=undefined;
+console.log(x);
+console.log(typeof(x));
+
+//func
+let sayHello=function(){    //camelCase
+    alert("Hello");
+}
+sayHello();
+
+//array
+let numArray=[1,2,3,4,5];
+let animals= new Array("cat","dog","rabbit");
+
+//object
+let person={
+    name:"John",
+    age:30,
+    title:"New York"
+};
+console.log(person.name);
+console.log(person.age);
+console.log(person.title);
+
+//symbol
+let sym1=Symbol("4");
+console.log(sym1);
+let sym2=Symbol("4");
+console.log(sym2);
+if(sym1===sym2){
+    console.log("true");
+}else{
+    console.log("false");
+}
+console.log(sym1===sym2); //false
