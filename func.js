5d41c194f4295e70992bac97ad2c72cbb7579e15
Ajit Pandey <ajitpandey4572@gmail.com>
2026-03-01 19:18:15 +0530

    Add interactive buttons and JavaScript functions for user interaction

---
+++ func.js
@@ -0,0 +1,44 @@
+let count = 0;
+document.write("Starting Loop"+"<br>");
+while (count < 10) {
+    document.write("Current Count: " + count + "<br>");
+    count++;
+}
+document.write("Loop Stopped!"+"<br>");
+
+let age = 20;
+if (age >= 18) {
+    document.write("You are an adult!"+"<br>");
+} else {    
+    document.write("You are a minor!"+"<br>");
+}
+
+let grade = 'A';
+document.write("Entering Switch Statement"+"<br>");
+switch (grade) {
+    case 'A':
+        document.write("Excellent!"+"<br>");
+        break;
+    case 'B':
+        document.write("Good!"+"<br>");
+        break;
+    case 'C':
+        document.write("Average!"+"<br>");
+        break;
+    case 'D':
+        document.write("Below Average!"+"<br>");
+        break;
+    case 'F':
+        document.write("Failed!"+"<br>");
+        break;
+    default:
+        document.write("Invalid Grade!"+"<br>");
+}
+//without parameter function
+function  myFunction() {
+    alert("Hello World!");
+}
+//with parameter function
+function SayHello(name,age) {
+    document.write(name + " is " + age + " years old!"+"<br>");
+}
