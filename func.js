62c1f813fa1bb90e5d15da6b691686d7dbf45f7d
Ajit Pandey <ajitpandey4572@gmail.com>
2026-03-10 09:37:15 +0530

    Add new object and array examples with console logging

--- func.js
+++ func.js
@@ -51,3 +51,44 @@ const person = {
 for(let x in person){
     document.write(x + ": " + person[x] + "<br>");
 }
+//arrow func
+let adding =(a,b)=>{
+    console.log(a+b);
+}
+adding(100,1);
+//object creation 
+let y=new Object();
+y.name="John";
+y.age=30;
+y.city="New York";
+//nested object 
+let  person1={
+    name:'jason',
+    age:22
+};
+person1.address={};
+person1.address.street="street1";
+person1['address']['flatNo']=16;
+
+
+//array
+let arr=[11,22,33,44,55];
+let myinfo= new Array();
+myinfo[0]="John";
+myinfo[1]=30;
+myinfo[2]="New York";
+let myinfo2=Array();
+console.log(myinfo2,myinfo);//both new ans not using new is the same
+
+
+let collection=[ {},[],true,"john",function(){},24,undefined,null,new String("hello"),new Date()];
+for(let a in collection){
+    console.log(collection[a]);
+}
+collection.techername="marking";
+collection.ph=122345678;
+console.log(collection.techername);
+console.log(collection.ph);
+console.log(collection.length);//length is 10 because it counts only the indexed elements not the properties
+collection.length=24;
+console.log(collection.length);//length is 24 but it does not change the indexed elements it just adds empty slots to the array
