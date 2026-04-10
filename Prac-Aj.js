955dd14a8fd12940475f94e462940ab23df7b036
Ajit Pandey <ajitpandey4572@gmail.com>
2026-04-10 11:37:21 +0530

    React package

---
+++ Prac-Aj.js
@@ -0,0 +1,29 @@
+function calculateGrade(){
+    let n=document.getElementById("subjects").value;
+
+    let total=0;
+
+    for(let i=1;i<=n;i++){
+        let marks=parseFloat(prompt("Enter marks for subject "+i));
+        total+=marks;
+    }
+
+    let average=total/n;
+
+    let grade;
+    if(average>=90){
+        grade="A";
+    }else if(average>=80){
+        grade="B";
+    }else if(average>=70){
+        grade="C";
+    }else if(average>=60){
+        grade="D";
+    }else{
+        grade="F";
+
+    }
+    let final= (average>=60) ? "Passed✅" : "Failed ❌";
+
+    document.getElementById("result").innerHTML="Total:"+total+"<br>Average: "+average+"<br>Grade: "+grade+"<br>Status: "+final;
+}
